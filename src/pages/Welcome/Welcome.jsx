88d9c9fbaf54a4608694bd158e8f4372e21f7a67
Italo Leal <itlcoorporativo@gmail.com>
2023-10-31 16:09:38 -0300

    feat: add imagens on Welcome page

--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -1,6 +1,9 @@
 import './Welcome.scss'
 import cat from '../../assets/SVGs/Welcome_Character.svg'
 import polygon from '../../assets/SVGs/Welcome_Polygon.svg'
+import checkGreen from '../../assets/SVGs/Check_Green.svg'
+import padlockGrey from '../../assets/SVGs/Padlock_Grey.svg'
+import padlockOrange from '../../assets/SVGs/Padlock_Orange.svg'
 
 export default function Welcome() {
   return (
@@ -8,22 +11,28 @@ export default function Welcome() {
       <h1>Welcome to the <span>Note Identification Game</span></h1>
       <main>
         <div className="intro">
-            <div className="bubble">
-              <div className="chat">
-                <p>Hey Superstars! Get ready for musical fun. Identify the note on the staff and move to the next level, with less than three mistakes. Learn note basics and become a pro!</p>
-                <p>Choose the level you want to play!</p>
-              </div>
-              <img src={polygon} />
+          <div className="bubble">
+            <div className="chat">
+              <p>Hey Superstars! Get ready for musical fun. Identify the note on the staff and move to the next level, with less than three mistakes. Learn note basics and become a pro!</p>
+              <p>Choose the level you want to play!</p>
             </div>
-            <img src={cat} className='cat' alt="Crescendo" />
+            <img src={polygon} />
+          </div>
+          <img src={cat} className='cat' alt="Crescendo" />
         </div>
         <div className="levels">
-            <button className='easy'><p>Easy</p>
-            </button>
-            <button className='medium'><p>Medium</p>
-            </button>
-            <button className='hard'><p>Hard</p>
-            </button>
+          <button className='easy'>
+            <p>Easy</p>
+            <img src={checkGreen} />
+          </button>
+          <button className='medium'>
+            <p>Medium</p>
+            <img src={padlockOrange} />
+          </button>
+          <button className='hard'>
+            <p>Hard</p>
+            <img src={padlockGrey} />
+          </button>
         </div>
       </main>
     </div>
