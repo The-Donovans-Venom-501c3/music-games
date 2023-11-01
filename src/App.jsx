2f5d3977152002b43b6aa1be6df83a3622459dee
Shubhankar Valimbe <shubhankarvalimbe@gmail.com>
2023-10-31 19:21:01 -0500

    Set up Jotai store

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
-import './assets/scss/App.scss'
-import Navbar from './components/Navbar/Navbar'
-import Loading from './pages/Quiz/Loading/Loading'
-import Welcome from './pages/Welcome/Welcome'
+import { useAtomValue } from "jotai";
+import { appStateAtom } from "./store/atoms";
+
+import "./assets/scss/App.scss";
+
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Quiz from "./pages/Quiz/Quiz";
+import GameFinished from "./pages/Game Finished/GameFinished";
+import Welcome from "./pages/Welcome/Welcome";
 
 function App() {
+  const appState = useAtomValue(appStateAtom);
 
   return (
     <>
-    <Navbar />
-    <Welcome />
-    {/* <Loading /> */}
+      <Navbar />
+      {appState === "home" ? (
+        <Home />
+      ) : appState === "welcome" ? (
+        <Welcome />
+      ) : appState === "quiz" ? (
+        <Quiz />
+      ) : (
+        <GameFinished />
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
