c87c8ff3e09832ce838bb674ab283f363999b924
ShimiRokach <shimi3601@gmail.com>
2023-11-01 16:46:33 +0200

    create branch

--- src/pages/Quiz/Loading/Loading.jsx
+++ src/pages/Quiz/Loading/Loading.jsx
@@ -5,16 +5,16 @@ import character from '../../../assets/svg/Loading_Character.svg'
 export default function Loading() {
   return (
     <div className='loading'>
-      <img src={character}  />
+      <img src={character} />
       <section>
-        <h1>Get Ready for Awesomeness!</h1>
+        <h1>Gett Ready for Awesomeness!</h1>
         <p>For each mission, you will be shown a music note.</p>
         <p>Choose one correct answer from options given.</p>
       </section>
       <section>
         <span>Directing you to the game in a minute...</span>
         <div className="loader">
-        <div className="circle">
+          <div className="circle">
             <div className="shine"></div>
           </div>
         </div>
