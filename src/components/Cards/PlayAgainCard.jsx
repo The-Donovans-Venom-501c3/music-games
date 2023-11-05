b5c864caa9f3b7d6a8bd404a26b11e5f4855e08f
grega1 <adrianogrego1@gmail.com>
2023-11-05 11:49:46 +0000

    feat: Play Again Card desktop layout

--- src/components/Cards/PlayAgainCard.jsx
+++ src/components/Cards/PlayAgainCard.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import "./Cards.scss";
+import character from "../../assets/svg/PlayAgain_Character.svg";
 
 export default function PlayAgainCard() {
   return (
     <div className="wrapper">
-      <div className="content centralized">
-        <img src="" alt="Character" className="" />
-        <p className="message">“Musicians don’t retire; they stop when there’s no more music in them.” 
-        <span>- Louis Armstrong</span>
-        </p>
+      <div className="content play-again centralized">
+        <img src={character} alt="Character" className="" />
+        <div className="quote centralized">
+          <p >
+            “Musicians <strong>don’t retire</strong> they stop when there’s no
+            more music in them.”
+          </p>
+          <span>- Louis Armstrong</span>
+        </div>
         <div className="button-container centralized">
-
-        <button className="primary" > NEXT LEVEL </button>
-        <button className="secondary" > SEE OTHER GAMES </button>
-        <button className="secondary" > SEE MY LESSONS </button>
+          <button className="primary"> NEXT LEVEL </button>
+          <button className="secondary"> SEE OTHER GAMES </button>
+          <button className="secondary"> SEE MY LESSONS </button>
         </div>
       </div>
     </div>
