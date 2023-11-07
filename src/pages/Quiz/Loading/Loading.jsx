8c25c98f2a2a2da16d8f49f1e891a4aa6dab09fe
ShimiRokach <shimi3601@gmail.com>
2023-11-07 23:15:41 +0200

    add container to loading element

--- src/pages/Quiz/Loading/Loading.jsx
+++ src/pages/Quiz/Loading/Loading.jsx
@@ -4,6 +4,7 @@ import character from '../../../assets/svg/Loading_Character.svg'
 
 export default function Loading() {
   return (
+<<<<<<< HEAD
     <div className='loading'>
       <img src={character}  />
       <section>
@@ -16,9 +17,25 @@ export default function Loading() {
         <div className="loader">
         <div className="circle">
             <div className="shine"></div>
+=======
+    <div className='loadingContainer'>
+      < div className='loading' >
+        <img src={character} />
+        <section>
+          <h1>Get Ready for Awesomeness!</h1>
+          <p>For each mission, you will be shown a music note.</p>
+          <p>Choose one correct answer from options given.</p>
+        </section>
+        <section>
+          <span>Directing you to the game in a minute...</span>
+          <div className="loader">
+            <div className="circle">
+              <div className="shine"></div>
+            </div>
+>>>>>>> f072cd6 (add container to loading element)
           </div>
-        </div>
-      </section>
+        </section>
+      </div >
     </div>
   )
 }
