62fb7c304c694ecb80309d2cd0a2d31ac17d8332
Italo Leal <itlcoorporativo@gmail.com>
2023-11-15 14:07:45 -0300

    style: Fix WelcomeScreen responsive

--- src/pages/Welcome/Welcome.jsx
+++ src/pages/Welcome/Welcome.jsx
@@ -4,28 +4,30 @@ import polygon from '../../assets/svg/Welcome_Polygon.svg'
 
 export default function Welcome() {
   return (
-    <div className='welcome'>
-      <h1>Welcome to the <span>Note Identification Game</span></h1>
-      <main>
-        <div className="intro">
-            <div className="bubble">
-              <div className="chat">
-                <p>Hey Superstars! Get ready for musical fun. Identify the note on the staff and move to the next level, with less than three mistakes. Learn note basics and become a pro!</p>
-                <p>Choose the level you want to play!</p>
-              </div>
-              <img src={polygon} />
+      <div className='container'>
+        <div className='welcome'>
+          <h1>Welcome to the <span>Note Identification Game</span></h1>
+          <main>
+            <div className="intro">
+                <div className="bubble">
+                  <div className="chat">
+                    <p>Hey Superstars! Get ready for musical fun. Identify the note on the staff and move to the next level, with less than three mistakes. Learn note basics and become a pro!</p>
+                    <p>Choose the level you want to play!</p>
+                  </div>
+                  <img src={polygon} />
+                </div>
+                <img src={cat} className='cat' alt="Crescendo" />
             </div>
-            <img src={cat} className='cat' alt="Crescendo" />
+            <div className="levels">
+                <button className='easy'><p>Easy</p>
+                </button>
+                <button className='medium'><p>Medium</p>
+                </button>
+                <button className='hard'><p>Hard</p>
+                </button>
+            </div>
+          </main>
         </div>
-        <div className="levels">
-            <button className='easy'><p>Easy</p>
-            </button>
-            <button className='medium'><p>Medium</p>
-            </button>
-            <button className='hard'><p>Hard</p>
-            </button>
-        </div>
-      </main>
-    </div>
+      </div>
   )
 }
