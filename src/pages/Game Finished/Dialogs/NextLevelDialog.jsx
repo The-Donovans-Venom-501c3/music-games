62a7372995933bba717a68258e105b3cda2b5129
Hung Le <45841230+hungle2396@users.noreply.github.com>
2023-11-14 18:48:20 -0800

    Made responsive design for game-finished screens for tablet, small screen laptop, and big screen desktop.

--- src/pages/Game Finished/Dialogs/NextLevelDialog.jsx
+++ src/pages/Game Finished/Dialogs/NextLevelDialog.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import "./Dialogs.scss";
 
-import padlock from "../../assets/svg/NextLevel_Unlocked.svg"
+// import padlock from "../../assets/svg/NextLevel_Unlocked.svg"
+
+import padlock from "../../../assets/svg/Welcome_Character.svg";
 
 export default function NextLevelDialog() {
-  return (
-    <div className="wrapper">
-      <div className="content next-level centralized">
-        <img src={padlock} alt="padlock" className="padlock" />
-        <span className="message">{"You've unlocked the next level"}</span>
-        <div className="button-container centralized">
-
-        <button className="primary" > NEXT LEVEL </button>
-        <button className="secondary" > PLAY AGAIN </button>
-        <button className="secondary" > SEE OTHER GAMES </button>
+    return (
+        <div className="wrapper">
+            <div className="content next-level centralized">
+                <img src={padlock} alt="padlock" className="padlock" />
+                <span className="message">
+                    {"You've unlocked the next level"}
+                </span>
+                <div className="button-container centralized">
+                    <button className="primary"> NEXT LEVEL </button>
+                    <button className="secondary"> PLAY AGAIN </button>
+                    <button className="secondary"> SEE OTHER GAMES </button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
