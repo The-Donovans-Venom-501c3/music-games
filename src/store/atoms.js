00c79f9d74c33df82bf42181ffbc08d7c915992d
Shubhankar Valimbe <shubhankarvalimbe@gmail.com>
2023-11-21 05:59:37 -0600

    Removed modified file

--- src/store/atoms.js
+++ src/store/atoms.js
@@ -1,9 +1,13 @@
 import { atom } from 'jotai'
 
 export const appStateAtom = atom("welcome");
-// export const loadingStateAtom = atom("loading");
-export const loadingStateAtom = atom("quiz");
-export const quizStateAtom = atom("overlay");
+export const loadingStateAtom = atom("loading");
+/*
+Possible Loading states:
+1) loading
+2) quiz
+*/
+export const quizStateAtom = atom("quiz");
 /*
 Possible Quiz states:
 1) quiz
@@ -18,3 +22,7 @@ Possible Affirmation states:
 2) fail
 3) tryAgain
 */
+export const timerAtom = atom(0);  // starts from 0 and keeps incrementing
+export const livesAtom = atom(3); // starts from 3, decrements till 0
+export const questionAtom = atom(1); // goes till the last question
+export const scoreAtom = atom(0); // min = 0, max = no. of questions
