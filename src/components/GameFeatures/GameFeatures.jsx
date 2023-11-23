a61d90916bc2cbca22dc27d4987cf3840b21a749
Randa-Alfar <my.violin.ru@gmail.com>
2023-11-23 23:06:17 +0200

    fixed toggles

--- src/components/GameFeatures/GameFeatures.jsx
+++ src/components/GameFeatures/GameFeatures.jsx
@@ -1,6 +1,11 @@
 import './GameFeatures.scss'
 import musicnote from '../../assets/svg/MusicNote.svg'
+import Switch from '@mui/material/Switch';
+import Slider from '@mui/material/Slider';
+import React, { useState } from 'react'
+
 const GameFeatures = () =>{
+    
     return(
         <div className='gameFeatureContainer'>
             <div className='gameLives'>
@@ -25,18 +30,60 @@ const GameFeatures = () =>{
                 <div>
                     <span>Music</span>
                     <div id='music'>
-                        <div id="musicBtn"></div>
-                        <div value="off">OFF</div>
+                        <Switch color='success'
+                        sx={{ 
+                            '& .MuiSwitch-thumb':{
+                                borderRadius:'5px',
+                                height:'5vh',
+                                backgroundColor:'$grey-100',
+                                border:'2px solid $grey-500',
+                                width:'3.5vw',
+                                transform: 'translateX(0px)',
+                                boxShadow:'0px 5px 0px 0px #BFBABB',
+
+                            },
+                            '& .MuiSwitch-track':{
+                                borderRadius:'5px',
+                                width:'25vw'
+                            },
+                            '.css-1xvpzln-MuiButtonBase-root-MuiSwitch-switchBase.Mui-checked':{
+                                '-webkit-transform': 'translateX(3.5vw)',
+                                '-moz-transform': 'translateX(3.5vw)',
+                               ' -ms-transform': 'translateX(3.5vw)',
+                                transform: "translateX(3.5vw)",
+                                color:'#F0EEEE'
+                            },
+                            width:'8vw',
+                            height:'8.5vh',
+                            
+                        }}
+                        />
                     </div>
                 </div>
                 <div>
                     <span>Volume</span>
                     <div id='volume'>
-                          <input type="range" className="slider" min="1" max="100" value="1"/>
+                        <Slider
+                        defaultValue={30}
+                        valueLabelDisplay="auto"
+                        sx={{
+                            width: "5vw",
+                            '.css-eg0mwd-MuiSlider-thumb':{
+                                color:'#F0EEEE',
+                            },
+                            '& .css-2bajgq-MuiSlider-root':{
+                                color:'#F0EEEE',
+                            },
+                            '.css-1gv0vcd-MuiSlider-track':{
+                                color:'#BFBABB',
+                                height:'1vh',
+                            }
+                        }}
+                        />
                     </div>
                 </div>
             </div >
         </div>
     );
 }
-export default GameFeatures
+export default GameFeatures;
