80b5e8a3955399db01e629264111b998ffb4c431
Italo Leal <itlcoorporativo@gmail.com>
2023-11-23 16:25:30 -0300

    feat: Add volume state and ref at gamefeatures

--- src/components/GameFeatures/GameFeatures.jsx
+++ src/components/GameFeatures/GameFeatures.jsx
@@ -1,13 +1,34 @@
 import "./GameFeatures.scss";
 import musicnote from "../../assets/svg/MusicNote.svg";
-// import { MusicToggle } from "./Music/MusicToggle";
+import dot from "../../assets/svg/Dot.svg"
+//import { MusicToggle } from "./Music/MusicToggle";
 import Timer from "./Timer/Timer";
+import { useRef, useState } from "react";
 import { useAtomValue } from "jotai";
 import { livesAtom, scoreAtom } from "../../store/atoms";
 
 const GameFeatures = () => {
   const score = useAtomValue(scoreAtom);
   const lives = useAtomValue(livesAtom);
+  const [musicOn, setMusicOn] = useState(false);
+  const audioRef = useRef(null);
+  const MAX = 20;
+
+  const toggleMusic = () => {
+    if(musicOn){
+      audioRef.current?.pause();
+      setMusicOn(!musicOn)
+    } else {
+      audioRef.current?.play();
+      setMusicOn(!musicOn)
+    }
+  };
+
+  function handleVolume(e) {
+    const { value } = e.target;
+    const volume = Number(value) / MAX;
+    audioRef.current.volume = volume;
+  }
 
   return (
     <div className="gameFeatureContainer">
@@ -30,27 +51,29 @@ const GameFeatures = () => {
         </div>
       </div>
       <div className="gameSetting">
-        {/* <MusicToggle /> */}
+        {/* <MusicToggle audioRef={audioRef}/> */}
         <div>
-           <span>Music</span>
-           <div id="music">
-             <div id="musicBtn"></div>
-             <div value="off">OFF</div>
-           </div>
-         </div>
+            <span>Music</span>
+    
+            <div id="music" className= {musicOn ? "on" : "" }>
+            <button
+                onClick={() => {
+                toggleMusic()
+                }}
+            >
+                <img src={dot} alt="play music toggle" />
+            </button>
+            <span>{musicOn? "ON" : "OFF"}</span>
+            </div>
+        </div>
         <div>
           <span>Volume</span>
           <div id="volume">
-            <input
-              type="range"
-              className="slider"
-              min="1"
-              max="100"
-              value="1"
-            />
+            <input className="slider" type="range" min="0" max={MAX} onChange={(e) => handleVolume(e)}/>
           </div>
         </div>
       </div>
+      <audio ref={audioRef} loop src={"/discord-notification.mp3"} />
     </div>
   );
 };
