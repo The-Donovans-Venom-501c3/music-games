e8648a0b4617e75637af3029e7744ca2ab408dc5
grega1 <adrianogrego1@gmail.com>
2023-11-26 17:45:17 +0000

    fix: music toggle style

--- src/components/GameFeatures/GameFeatures.jsx
+++ src/components/GameFeatures/GameFeatures.jsx
@@ -1,12 +1,12 @@
 import "./GameFeatures.scss";
 // import { MusicToggle } from "./Music/MusicToggle";
 import musicnote from '../../assets/svg/MusicNote.svg'
-import Switch from '@mui/material/Switch';
 import Slider from '@mui/material/Slider';
 import React, { useState } from 'react'
 import Timer from "./Timer/Timer";
 import { useAtomValue } from "jotai";
 import { livesAtom, scoreAtom } from "../../store/atoms";
+import { MusicToggle } from "./Music/MusicToggle";
 
 const GameFeatures = () => {
   const score = useAtomValue(scoreAtom);
@@ -33,39 +33,7 @@ const GameFeatures = () => {
         </div>
       </div>
             <div className='gameSetting'>
-                <div>
-                    <span>Music</span>
-                    <div id='music'>
-                        <Switch color='success'
-                        sx={{ 
-                            '& .MuiSwitch-thumb':{
-                                borderRadius:'5px',
-                                height:'5vh',
-                                backgroundColor:'$grey-100',
-                                border:'2px solid $grey-500',
-                                width:'3.5vw',
-                                transform: 'translateX(0px)',
-                                boxShadow:'0px 5px 0px 0px #BFBABB',
-
-                            },
-                            '& .MuiSwitch-track':{
-                                borderRadius:'5px',
-                                width:'25vw'
-                            },
-                            '.css-1xvpzln-MuiButtonBase-root-MuiSwitch-switchBase.Mui-checked':{
-                                '-webkit-transform': 'translateX(3.5vw)',
-                                '-moz-transform': 'translateX(3.5vw)',
-                               ' -ms-transform': 'translateX(3.5vw)',
-                                transform: "translateX(3.5vw)",
-                                color:'#F0EEEE'
-                            },
-                            width:'8vw',
-                            height:'8.5vh',
-                            
-                        }}
-                        />
-                    </div>
-                </div>
+              <MusicToggle />
                 <div>
                     <span>Volume</span>
                     <div id='volume'>
