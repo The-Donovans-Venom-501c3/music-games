4816194044cc2e19970c0895c9fef5f0623edb95
Italo Leal <itlcoorporativo@gmail.com>
2023-11-28 01:56:08 -0300

    feat: add formlabel at gamefeatures

--- src/components/GameFeatures/GameFeatures.jsx
+++ src/components/GameFeatures/GameFeatures.jsx
@@ -7,6 +7,7 @@ import Timer from "./Timer/Timer";
 import { useRef } from "react";
 import { useAtomValue, useAtom } from "jotai";
 import { livesAtom, scoreAtom, musicStateAtom, levelStateAtom } from "../../store/atoms";
+import { FormControlLabel } from "@mui/material";
 
 const GameFeatures = () => {
   const score = useAtomValue(scoreAtom);
@@ -58,36 +59,42 @@ const GameFeatures = () => {
                 <div>
                     <span>Music</span>
                     <div id='music'>
-                        <Switch color='success'
-                        sx={{ 
-                            '& .MuiSwitch-thumb':{
-                                borderRadius:'5px',
-                                height:'5vh',
-                                backgroundColor:'$grey-100',
-                                border:'2px solid $grey-500',
-                                width:'3.5vw',
-                                transform: 'translateX(0px)',
-                                boxShadow:'0px 5px 0px 0px #BFBABB',
-
-                            },
-                            '& .MuiSwitch-track':{
-                                borderRadius:'5px',
-                                width:'25vw'
-                            },
-                            '.css-1xvpzln-MuiButtonBase-root-MuiSwitch-switchBase.Mui-checked':{
-                                '-webkit-transform': 'translateX(3.5vw)',
-                                '-moz-transform': 'translateX(3.5vw)',
-                               ' -ms-transform': 'translateX(3.5vw)',
-                                transform: "translateX(3.5vw)",
-                                color:'#F0EEEE'
-                            },
-                            width:'8vw',
-                            height:'8.5vh',
-                            
-                        }}
-                        checked={musicOn}
-                        onChange={handleChangeSwitch}
+                        <FormControlLabel
+                          control={
+                            <Switch color='success'
+                            sx={{ 
+                                '& .MuiSwitch-thumb':{
+                                    borderRadius:'5px',
+                                    height:'5vh',
+                                    backgroundColor:'$grey-100',
+                                    border:'2px solid $grey-500',
+                                    width:'3.5vw',
+                                    transform: 'translateX(0px)',
+                                    boxShadow:'0px 5px 0px 0px #BFBABB',
+    
+                                },
+                                '& .MuiSwitch-track':{
+                                    borderRadius:'5px',
+                                    width:'25vw'
+                                },
+                                '.css-1xvpzln-MuiButtonBase-root-MuiSwitch-switchBase.Mui-checked':{
+                                    '-webkit-transform': 'translateX(3.5vw)',
+                                    '-moz-transform': 'translateX(3.5vw)',
+                                   ' -ms-transform': 'translateX(3.5vw)',
+                                    transform: "translateX(3.5vw)",
+                                    color:'#F0EEEE'
+                                },
+                                width:'7vw',
+                                height:'8.5vh',
+                                
+                            }}
+                            checked={musicOn}
+                            onChange={handleChangeSwitch}
+                            />
+                          }
+                          label={musicOn ? 'ON' : 'OFF'}                        
                         />
+
                     </div>
                 </div>
                 <div>
@@ -98,7 +105,7 @@ const GameFeatures = () => {
                         onChange={(e) => handleChangeVolume(e)}                        
                         valueLabelDisplay="auto"
                         sx={{
-                            width: "5vw",
+                            width: "7vw",
                             '.css-eg0mwd-MuiSlider-thumb':{
                                 color:'#F0EEEE',
                             },
