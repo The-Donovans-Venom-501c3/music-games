0805e38366031fa81d1a76b8784da899e9462355
Hung Le <45841230+hungle2396@users.noreply.github.com>
2023-11-29 22:07:10 -0800

    Made the selected note changed color to yellow like the hovered button.

--- src/components/QuizSection/Options/ScaleOptions/ScaleOptions.jsx
+++ src/components/QuizSection/Options/ScaleOptions/ScaleOptions.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "./ScaleOptions.scss";
 import sharp from "../../../../assets/svg/SharpSymbol.svg";
 import bimol from "../../../../assets/svg/BimolSymbol.svg";
 
@@ -7,59 +8,70 @@ const notes2 = ["C#", "D♭", "E♭", "F#", "G#", "A♭", "B♭"];
 const chords = ["Major", "Natural", "Harmonic", "Melodic"];
 
 export default function ScaleOptions({ handleOptionClick, level }) {
-  const [note, setNote] = useState("");
+    const [selectedNote, setSelectedNote] = useState("");
 
-  const handleChordClick = (chord) => {
-    if (note) {
-      handleOptionClick(note + " " + chord);
-    }
-  };
+    const handleNoteClick = (note) => {
+        setSelectedNote(note);
+    };
 
-  return (
-    <>
-      <div className="small-btn-wrapper">
-        {notes.map((note, index) => (
-          <button
-            className="option-btn small-btn"
-            key={index}
-            onClick={() => setNote(note)}
-            style={{height:45}}
-          >
-            {note}
-          </button>
-        ))}
-        {notes2.map((note, index) =>
-          level == "easy" ? (
-            <div className="option-blank" key={index} style={{height:45}}></div>
-          ) : (
-            <button
-              className="option-btn small-btn"
-              key={index}
-              onClick={() => setNote(note)}
-              style={{height:45}}
-            >
-              {note[0]}
-              {note[1] == "#" ? (
-                <img src={sharp}></img>
-              ) : (
-                <img src={bimol}></img>
-              )}
-            </button>
-          )
-        )}
-      </div>
-      <div className="large-btn-wrapper" style={{marginTop:20}}>
-        {chords.map((chord, index) => (
-          <button
-            className="option-btn large-btn"
-            key={index}
-            onClick={() => handleChordClick(chord)}
-            
-          >
-            {chord == "Major" ? chord : chord + " Minor"}
-          </button>
-        ))}
-      </div>
-    </>
-  );
+    const handleChordClick = (chord) => {
+        if (selectedNote) {
+            handleOptionClick(selectedNote + " " + chord);
+        }
+    };
+
+    return (
+        <>
+            <div className="small-btn-wrapper">
+                {notes.map((note, index) => (
+                    <button
+                        className={`option-btn small-btn ${
+                            note === selectedNote ? "selected-note" : ""
+                        }`}
+                        key={index}
+                        onClick={() => handleNoteClick(note)}
+                        style={{ height: 45 }}
+                    >
+                        {note}
+                    </button>
+                ))}
+                {notes2.map((note, index) =>
+                    level == "easy" ? (
+                        <div
+                            className="option-blank"
+                            key={index}
+                            style={{ height: 45 }}
+                        ></div>
+                    ) : (
+                        <button
+                            className={`option-btn small-btn ${
+                                note === selectedNote ? "selected-note" : ""
+                            }`}
+                            key={index}
+                            onClick={() => handleNoteClick(note)}
+                            style={{ height: 45 }}
+                        >
+                            {note[0]}
+                            {note[1] == "#" ? (
+                                <img src={sharp}></img>
+                            ) : (
+                                <img src={bimol}></img>
+                            )}
+                        </button>
+                    )
+                )}
+            </div>
+            <div className="large-btn-wrapper" style={{ marginTop: 20 }}>
+                {chords.map((chord, index) => (
+                    <button
+                        className="option-btn large-btn"
+                        key={index}
+                        onClick={() => handleChordClick(chord)}
+                    >
+                        {chord == "Major" ? chord : chord + " Minor"}
+                    </button>
+                ))}
+            </div>
+        </>
+    );
 }
