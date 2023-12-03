41d5a92e322f3ca82882a48cd7806325adc674ed
Shubhankar Valimbe <shubhankarvalimbe@gmail.com>
2023-12-03 14:13:07 -0600

    fixed key signature questions

--- src/utils/questions.js
+++ src/utils/questions.js
@@ -3,21 +3,22 @@ import data from '../data/data.json';
 const gameIdx = {'note':0, 'key':1, 'chord':2, 'scale':3};
 const levelIdx = {'easy':0, 'medium':1, 'hard':2};
 
-function getRandomQuestions(questions, number) {
+function getRandomQuestions(questions, length) {
 
   // Shuffle the questions using the Fisher-Yates algorithm
-  for (let i = number; i > 0; i--) {    
-    const j = Math.floor((Math.random()*100)) % (number+1);
+  for (let i = length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
     [questions[i], questions[j]] = [questions[j], questions[i]];
   }
 
-  return questions.slice(0, number);
+  return questions.slice(0, length);
 }
 
 export function getQuestions(game, level) {
     const gameData = data.musicGames[gameIdx[game]].levels
     const numOfQuestionsPerLevel = gameData[levelIdx[level]].numberOfQuestions;
     const questions = [];
+
     for (const [level, numQuestions] of Object.entries(numOfQuestionsPerLevel)) {
       questions.push(...getRandomQuestions(gameData[levelIdx[level]].questions, numQuestions));
     }
