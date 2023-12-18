5f09c075eb51fd7221e68e5143abbcd92132d584
grega1 <adrianogrego1@gmail.com>
2023-12-18 20:53:41 +0000

    Revert "fix: cat animation"
    This reverts commit 2a87d35214eb603c0b932a5e4cb7b63fd1120e90.

--- src/pages/Quiz/QuizScreen/QuizScreen.jsx
+++ src/pages/Quiz/QuizScreen/QuizScreen.jsx
@@ -14,7 +14,7 @@ import {
   overlayAtom,
   quizStateAtom,
   timerOnAtom,
-  popupAtom,
+  popupAtom
 } from "../../../store/atoms";
 
 const QuizScreen = () => {
@@ -30,7 +30,7 @@ const QuizScreen = () => {
 
   const handlePause = () => {
     setTimerOn(!timerOn);
-    setPopup("pause");
+    setPopup('pause');
     setQuizState("popup");
   };
 
@@ -59,7 +59,7 @@ const QuizScreen = () => {
       </div>
 
       <div className="catConatiner">
-        <img className="cat" src={cat} />
+        <img id="cat" src={cat} />
       </div>
 
       <div className="GameScreen">
