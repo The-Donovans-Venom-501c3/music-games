c9d39f6a895bab690899d87e17b6227264e3048b
Hung Le <hungle2396@gmail.com>
2024-01-12 15:11:36 -0800

    fixed some responsiveness for the Big Laptop or desktop in QuizScreen

--- src/components/QuizSection/Options/KeyOptions/KeyOptions.jsx
+++ src/components/QuizSection/Options/KeyOptions/KeyOptions.jsx
@@ -1,28 +1,33 @@
-import sharp from "../../../../assets/svg/SharpSymbol.svg";
-import bimol from "../../../../assets/svg/BimolSymbol.svg";
+import sharp from '../../../../assets/svg/SharpSymbol.svg';
+import bimol from '../../../../assets/svg/BimolSymbol.svg';
 
+const notes = ['C#', 'D', 'E', 'F', 'G', 'A', 'B'];
+const notes2 = ['C♭', 'D♭', 'E♭', 'F#', 'G♭', 'A♭', 'B♭'];
 
-const notes = ["C#", "D", "E", "F", "G", "A", "B"];
-const notes2 = ["C♭", "D♭", "E♭", "F#", "G♭", "A♭", "B♭"];
-
-export default function KeyOptions({handleOptionClick}) {
+export default function KeyOptions({ handleOptionClick }) {
   return (
-    <div className='small-btn-wrapper' style={{marginTop:50}}>
-
+    <div className='small-btn-wrapper' style={{ marginTop: 50 }}>
       {notes.map((note, index) => (
-        <button style={{height:65}} className="option-btn small-btn" onClick={() => handleOptionClick(note)} key={index}>
+        <button
+          className='option-btn small-btn'
+          onClick={() => handleOptionClick(note)}
+          key={index}
+        >
           {note[0]}
           {note == 'C#' ? <img src={sharp}></img> : null}
         </button>
       ))}
 
       {notes2.map((note, index) => (
-        <button style={{height:65}} className="option-btn small-btn" onClick={() => handleOptionClick(note)} key={index}>
+        <button
+          className='option-btn small-btn'
+          onClick={() => handleOptionClick(note)}
+          key={index}
+        >
           {note[0]}
           {note == 'F#' ? <img src={sharp}></img> : <img src={bimol}></img>}
         </button>
       ))}
-
     </div>
-  )
+  );
 }
