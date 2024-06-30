ddb03a2c7a7d62fc9e3921f2e44217e0e00cf57c
Adriano Grego <adrianogrego1@gmail.com>
2024-06-30 23:59:33 +0100

    feat: isolating games data

--- src/utils/questions.js
+++ src/utils/questions.js
@@ -1,13 +1,31 @@
-import data from "../data/data.json";
-const gameIdx = {
-  note: 0,
-  key: 1,
-  "major-minor": 2,
-  scale: 3,
-  interval: 4,
-  chord: 5,
-  ledger: 7,
-};
+// import data from "../data/data.json";
+import chord from "../data/chord-game.json"
+import intervalIdentification from "../data/interval-identification.json"
+import keySignatureIdentification from "../data/key-signature-identification.json"
+import ledgerLineAddition from "../data/ledger-line-addition.json"
+import majorMinorIdentification from "../data/major-minor-identification.json"
+import noteIdentification from "../data/note-identification.json"
+import scaleIdentification from "../data/scale-identification.json"
+
+// const gameIdx = {
+//   note: 0,
+//   key: 1,
+//   "major-minor": 2,
+//   scale: 3,
+//   interval: 4,
+//   chord: 5,
+//   ledger: 7,
+// };
+const games={
+  "note": noteIdentification,
+  "key": keySignatureIdentification,
+  "major-minor":majorMinorIdentification,
+  "scale": scaleIdentification,
+  "interval": intervalIdentification,
+  "chord": chord,
+  "legder":ledgerLineAddition
+
+}
 const levelIdx = { easy: 0, medium: 1, hard: 2 };
 
 function getRandomQuestions(questions, number, game) {
@@ -36,10 +54,13 @@ function getRandomQuestions(questions, number, game) {
     return ques;
   }
 }
-
 export function getQuestions(game, level) {
-  const gameData = data.musicGames[gameIdx[game]].levels;
-  const numOfQuestionsPerLevel = gameData[levelIdx[level]].numberOfQuestions;
+  console.log(game)
+  const gameData = games[game]
+  console.log(gameData);
+  const gameLevel = gameData.levels
+  console.log(gameLevel)
+  const numOfQuestionsPerLevel =gameLevel[levelIdx[level]].numberOfQuestions;
   const questions = [];
 
   for (const [level, numQuestions] of Object.entries(numOfQuestionsPerLevel)) {
@@ -55,3 +76,23 @@ export function getQuestions(game, level) {
   // console.log(questions);
   return questions;
 }
+
+// export function getQuestions(game, level) {
+//   console.log(game);
+//   const gameData = data.musicGames[gameIdx[game]].levels;
+//   const numOfQuestionsPerLevel = gameData[levelIdx[level]].numberOfQuestions;
+//   const questions = [];
+
+//   for (const [level, numQuestions] of Object.entries(numOfQuestionsPerLevel)) {
+//     questions.push(
+//       ...getRandomQuestions(
+//         gameData[levelIdx[level]].questions,
+//         numQuestions,
+//         game
+//       )
+//     );
+//   }
+
+//   // console.log(questions);
+//   return questions;
+// }
